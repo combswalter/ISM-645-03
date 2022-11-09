b3954a865a72825faf171c4114e719e21d230a14
jroglesb <55505834+jroglesb@users.noreply.github.com>
2022-11-09 18:52:26 -0500

    Add files via upload

--- R/Extra Variables 1 - Jeremy.R
+++ R/Extra Variables 1 - Jeremy.R
@@ -1,17 +1,12 @@
 library(readxl)
+library(tidyverse)
+library(dplyr)
+
 inventory <- read.csv("RDC_Inventory_Core_Metrics_State_History.csv")
 inventory_nc <- inventory %>% filter(state_id == "NC")
 inventory_nc <- inventory_nc %>% rename(month = month_date_yyyymm)
 inventory_nc$month <- paste0(inventory_nc$month, "01")
-
-x <- c("202201","202202", "202203")
-x <- paste0(x, "01")
-x <- as.Date(x, format = "%Y%m%d")
-x <- as.POSIXct(x, format = "%Y%m%d")
 inventory_nc$month <- as.Date(inventory_nc$month, format = "%Y%m%d")
-
-library(tidyverse)
-library(dplyr)
 
 #Compare median days on the market to the median listing price/sq ft for all the US 
 ggplot(inventory, aes(x=median_listing_price_per_square_foot, y=median_days_on_market))+
@@ -36,7 +31,7 @@ ggplot(inventory_nc, aes(x=median_listing_price_per_square_foot, y=median_days_o
   ggtitle("NC Median Listing Price/sq. ft by Active Listings")
 
 #Compare median listing price/sq. ft to active listings for North Carolina
-ggplot(inventory_nc, aes(x=active_listing_count, y=median_listing_price_per_square_foot))+
+ggplot(inventory_nc, aes(x=active_listing_count, y=median_listing_price_per_square_foot, color=month))+
   geom_point()+
   scale_x_log10()+
   scale_y_log10()+
@@ -59,5 +54,11 @@ ggplot(inventory_nc, aes(y=median_days_on_market, x=month, color=month))+
   geom_smooth(method = "lm", se=FALSE)+
   ggtitle("NC Median Listing Price/sq. ft by Month")
 
-regression1 <- lm(median_listing_price_per_square_foot ~ active_listing_count + month, data=inventory_nc)
+# It appears that the month/year and median_days_on_market are strong variables for mean_listing_price_per_square_foot
+regression1 <- lm(median_listing_price_per_square_foot ~ active_listing_count + median_days_on_market + month, data=inventory_nc)
 summary(regression1)
+
+# There is a correlation between month/year and median_days_on_market but median_listing_price_per_square_foot is
+# a much better predictor
+regression2 <- lm(median_days_on_market ~ active_listing_count + median_listing_price_per_square_foot + month, data=inventory_nc)
+summary(regression2)
